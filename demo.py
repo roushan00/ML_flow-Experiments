404b9b0ced4e9a9debdfe229c1df4262d2916639
roushan00 <roushan-06534@gmail.com>
2025-03-27 12:54:49 +0530

    Changes addded

---
+++ demo.py
@@ -0,0 +1,30 @@
+import os
+import warnings
+import sys
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+
+from urllib.parse import urlparse
+import mlflow
+from mlflow.models.signature import infer_signature
+import mlflow.sklearn
+import dagshub
+import logging
+
+import dagshub
+dagshub.init(repo_owner='roushan06534', repo_name='ML_flow-Experiments', mlflow=True)
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
